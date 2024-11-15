4c5a6d914a51936b568797a835ac2a77aed0fec2
tkadziolka <tmax0135@gmail.com>
2024-11-15 08:11:27 +0100

    Bumped flutter, gradle plugin and cleaned code

--- example/linux/flutter/generated_plugin_registrant.cc
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -6,10 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <kode_view/kode_view_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
-  g_autoptr(FlPluginRegistrar) kode_view_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "KodeViewPlugin");
-  kode_view_plugin_register_with_registrar(kode_view_registrar);
 }
--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <kode_view/kode_view_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  KodeViewPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("KodeViewPluginCApi"));
 }

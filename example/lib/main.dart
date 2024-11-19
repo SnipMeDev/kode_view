fe9a2d2f8b55030778100213c8a54eb8806008ce
tkadziolka <tmax0135@gmail.com>
2024-11-19 07:26:42 +0100

    Improved example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final SyntaxHighlightingController controller =
       SyntaxHighlightingController(text: codeSnippet);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,32 +27,40 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Kode View example'),
         ),
         body: Center(
-            child: Column(
-          children: [
-            const Text("CodeTextView"),
-            const SizedBox(
-              height: 12,
-            ),
-            CodeTextView(
-              code: codeSnippet,
-              language: "Java",
-              theme: "darcula",
-              options: TextSelectionOptions(
-                copy: true,
-                selectAll: true,
-                share: true,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text("CodeTextView"),
+              const SizedBox(
+                height: 12,
               ),
-            ),
-            const SizedBox(height: 24),
-            const Text("CodeEditText"),
-            const SizedBox(height: 12),
-            CodeEditText(
-              code: codeSnippet,
-              controller: controller,
-              showCursor: true,
-            ),
-          ],
-        )),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: CodeTextView(
+                  code: codeSnippet,
+                  language: "Java",
+                  theme: "darcula",
+                  options: TextSelectionOptions(
+                    copy: true,
+                    selectAll: true,
+                    share: true,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24),
+              const Text("CodeEditText"),
+              const SizedBox(height: 12),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: CodeEditText(
+                  code: codeSnippet,
+                  controller: controller,
+                  showCursor: true,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
